0923c386c11c19ba7a7ca67761acd23158b3a83b
Chris Warren <chris.warren.2019@gmail.com>
2023-10-20 16:53:44 -0400

    Similar Movies

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,11 +1,21 @@
-import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { supabase } from "../lib/supabase";
+import MovieItem from "../components/MovieItem";
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
   const [movie, setMovie] = useState(null);
+  const [similarMovies, setSimilarMovies] = useState(null);
   useEffect(() => {
     const fetchMovie = async () => {
       if (!id) {
@@ -22,16 +32,32 @@ const MovieDetails = () => {
       }
     };
     fetchMovie();
-  }, []);
+  }, [id]);
+  useEffect(() => {
+    if (!movie?.embedding) {
+      return;
+    }
+    const fetchSimilarMovies = async () => {
+      const { data } = await supabase.rpc("match_movies", {
+        query_embedding: movie.embedding, // Pass the embedding you want to compare
+        match_threshold: 0.78, // Choose an appropriate threshold for your data
+        match_count: 5, // Choose the number of matches
+      });
+      setSimilarMovies(data);
+    };
+    fetchSimilarMovies();
+  }, [movie?.embedding]);
   if (!movie) {
     return <ActivityIndicator />;
   }
+  console.log(similarMovies);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{movie.title}</Text>
       <Text style={styles.subtitle}>{movie.tagline}</Text>
       <Text style={styles.overview}>{movie.overview}</Text>
       <Text style={styles.similar}>Similar Movies:</Text>
+      <FlatList data={similarMovies} renderItem={MovieItem} />
     </View>
   );
 };
@@ -40,6 +66,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   title: {
     fontSize: 20,
